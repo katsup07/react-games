5f44f8da50a91e02449ea65195c02d2e4ea11dea
katsup <brett_7y7@yahoo.ca>
2023-04-14 21:57:43 +0900

    Add genre list

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,8 @@
+import useGenres from './../hooks/useGenres';
+
+const GenreList = () => {
+  const {genres, error, isLoading} = useGenres();
+  return (<ul>{genres.map(g => <li key={g.id}>{g.name}</li>)}</ul>  );
+}
+ 
+export default GenreList;
