0973ba7a7491c161b2c3c8a41b0b3e4f76f109f2
katsup <brett_7y7@yahoo.ca>
2023-04-19 00:03:17 +0900

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from '../assets/no-image-placeholder.webp';
+
 const getCroppedImageUrl = (url: string) => {
-  if(!url) return '';
-  
+  if(!url) return noImage;
+
   const target = 'games/';
   const index = url.indexOf(target);
   return url.slice(0,index) + 'crop/600/400/' + url.slice(index);
