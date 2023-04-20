01d3d43bbd184150e2609f121041717a08f56f59
katsup <brett_7y7@yahoo.ca>
2023-04-20 23:41:04 +0900

    Modify chakra theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,16 @@ const config: ThemeConfig = {
   initialColorMode: 'dark', 
 };
 
-const theme = extendTheme({config});
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: { // Light Mode colors
+      50: '#ece9ffde',
+      100: '#c6bef7de',
+      200: '#9f95ecde',
+      300: '#796ae2de',
+    }
+  }
+});
 
 export default theme;
